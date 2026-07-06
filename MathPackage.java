945faf37a0bc5525e3c4dd56223a6616bccac31a
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-06 19:40:11 +0530

    Practice: Worked on MathPackage.java

---
+++ MathPackage.java
@@ -0,0 +1,15 @@
+public class MathPackage {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        System.out.println(Math.max(x,y));
+        System.out.println(Math.min(x,y));
+        System.out.println(Math.sqrt(100));
+        System.out.println(Math.pow(10,2));
+        System.out.println(Math.abs(-5));
+        System.out.println(Math.floor(2.6));
+        System.out.println(Math.ceil(2.4));
+
+    }
+}
