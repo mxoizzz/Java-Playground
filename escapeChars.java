dd672a038e8ac96aa1e846792a24ee972128f164
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-06 19:32:19 +0530

    Practice: Worked on escapeChars.java

---
+++ escapeChars.java
@@ -0,0 +1,11 @@
+public class escapeChars {
+    public static void main(String[] args){
+        System.out.println("Java is \'Fun\'");
+        System.out.println("a\fb");
+        System.out.println("a\bb");
+        System.out.println("a\nb");
+        System.out.println("a\tb");
+        System.out.println("a\rb");
+
+    }
+}
