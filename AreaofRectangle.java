c733d814caa80694d8a56b8f108f7f1d9a7a9ca1
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-06 11:55:58 +0530

    Practice: Worked on AreaofRectangle.java

---
+++ AreaofRectangle.java
@@ -0,0 +1,9 @@
+public class AreaofRectangle {
+    public static void main(String[] args ){
+        int length = 5;
+        int width = 7;
+
+        int area = length * width ;
+        System.out.println("Area of Rectangle :"+ area);
+    }
+}
