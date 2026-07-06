88d7070a19565dee2cc5e89fd703714da1035372
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-06 18:14:59 +0530

    Practice: Worked on concat.java

---
+++ concat.java
@@ -0,0 +1,10 @@
+public class concat {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "Programming";
+
+        System.out.println(str1 + " " + str2);
+        System.out.println(str1 + str2);
+        System.out.println(str1.concat(str2));
+    }
+}
