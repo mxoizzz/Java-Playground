738b6d720d2b9f6f9c678e728afebee97b1f3cce
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-06 17:46:31 +0530

    Practice: Worked on operatorprecedence.java

---
+++ operatorprecedence.java
@@ -0,0 +1,18 @@
+public class operatorprecedence {
+    public static void main(String[] args) {
+        int ans = 2 + 3 -1;
+        System.out.println(ans);
+        
+        ans = 5 + (4 -1);
+        System.out.println(ans);
+        
+        ans = 4 + 5 * 3;
+        System.out.println(ans);
+        
+        ans = 64 / 4 * 1;
+        System.out.println(ans);
+
+
+
+    }
+}
