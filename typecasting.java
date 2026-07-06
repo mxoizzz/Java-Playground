c486437af7f8be43bb2dffb6864182e21020ae06
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-06 12:24:42 +0530

    Practice: Worked on typecasting.java

--- typecasting.java
+++ typecasting.java
@@ -1,11 +1,22 @@
 public class typecasting {
     public static void main(String[] args){
         // Widening
+        // done automatically
 
         int a = 7;
         double b = a;
 
         System.out.println(a); //int
         System.out.println(b); //converted to double
+
+        // Narrowing
+        // must be done manually
+
+        double x = 23.5;
+        int y = (int) x;
+
+        System.out.println(x);
+        System.out.println(y);
+
     }
 }
