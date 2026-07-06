772e46b6ef93595d0f405556113693df4fd7e308
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-06 12:18:24 +0530

    Practice: Worked on varkeyword.java

---
+++ varkeyword.java
@@ -0,0 +1,9 @@
+public class varkeyword {
+    public static void main(String[] args){
+        var x = 10;
+        System.out.println(x);
+
+        var y = 23.89;
+        System.out.println(y);
+    }
+}
