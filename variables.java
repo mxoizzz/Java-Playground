2acea199e421f0d51311e8b63474589b54cb4462
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-06 11:45:48 +0530

    Practice: Worked on variables.java

---
+++ variables.java
@@ -0,0 +1,18 @@
+public class variables {
+    public static void main(String[] args){
+        int num = 7;
+        String str = "Moiz Shaikh";
+        float numm = 3.5f;
+        double nummm = 7.1;
+        boolean isSuccess = true;
+        char ch = 'M';
+
+        System.out.println("Integer: "+num);
+        System.out.println("String: "+str);
+        System.out.println("Floating: "+numm);
+        System.out.println("Double: "+nummm);
+        System.out.println("Boolean: "+isSuccess);
+        System.out.println("Character: "+ch);
+
+    }
+}
