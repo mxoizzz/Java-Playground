44a872fec84641d966b668ddb4af29b3b3bed826
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-06 12:42:06 +0530

    Practice: Worked on logical.java

---
+++ logical.java
@@ -0,0 +1,9 @@
+public class logical {
+    public static void main(String[] args) {
+        int x = 7;
+        System.out.println((x<5) && (x<10));
+        System.out.println((x<5) || (x<10));
+        System.out.println(!(x<5));
+
+    }
+}
