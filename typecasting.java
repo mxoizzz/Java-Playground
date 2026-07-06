1a963f2e7e7e85110704b8575f1a287787a8dc6c
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-06 12:22:37 +0530

    Practice: Worked on typecasting.java

---
+++ typecasting.java
@@ -0,0 +1,11 @@
+public class typecasting {
+    public static void main(String[] args){
+        // Widening
+
+        int a = 7;
+        double b = a;
+
+        System.out.println(a); //int
+        System.out.println(b); //converted to double
+    }
+}
