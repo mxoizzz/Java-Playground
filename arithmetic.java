0b8744ca4024cd1f5297c30779a852206e5237d9
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-06 12:27:47 +0530

    Practice: Worked on arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,16 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(++b);
+        System.out.println(--b);
+    }
+}
