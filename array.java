253ab253adbc432a9c44e367f36ced8662231df8
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-07 09:45:13 +0530

    Practice: Worked on array.java

---
+++ array.java
@@ -0,0 +1,25 @@
+public class array {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        System.out.println(nums[1]);
+
+        System.out.println(nums[2]);
+        nums[2] = 10;
+        System.out.println(nums[2]);
+
+        System.out.println(nums.length);
+
+        String[] names = new String[3];
+
+        names[0] = "Moiz";
+        names[1] = "Ronaldo";
+        names[2] = "Messi";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+
+        int[] nums1 = new int[] {1,2,3,4,5};
+        int[] nums2 = {1,2,3,5,4};
+    }
+}
