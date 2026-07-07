328511d189f401598f6c81e83a911442fae9e50e
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-07 09:50:43 +0530

    Practice: Worked on array.java

--- array.java
+++ array.java
@@ -21,5 +21,22 @@ public class array {
 
         int[] nums1 = new int[] {1,2,3,4,5};
         int[] nums2 = {1,2,3,5,4};
+
+        for(int n:nums1){
+            System.out.println(n);
+        }
+        System.out.println("------------------------------------------");
+        for(int n:nums2){
+            System.out.println(n);
+        }
+
+        for(int i = 0;i<5;i++){
+            System.out.println(nums[i]);
+        }
+        int sum=0;
+        for(int i =0;i<5;i++){
+            sum+=nums[i];
+        }
+        System.out.println(sum);
     }
 }
